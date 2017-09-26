0c7e760eefc0df91c1c5d2e39d6af1b48ab0eddc
Raghu Betina <raghu@firstdraft.com>
2017-09-26 01:48:38 +0000

    Whitelist columns in BrokerProfile for ActiveAdmin

--- app/admin/broker_profile.rb
+++ app/admin/broker_profile.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register BrokerProfile do
 
+ permit_params :phone_work, :address_work, :city_work, :zip_work, :state_work, :country_work, :phone_mobile, :practice_name, :company, :insurer_id, :photo, :job_title
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
