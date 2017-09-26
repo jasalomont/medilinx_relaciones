eb25d2eba3e6657654f7fc5e6da6294194b59740
Raghu Betina <raghu@firstdraft.com>
2017-09-26 01:48:29 +0000

    Whitelist columns in Rx for ActiveAdmin

--- app/admin/rx.rb
+++ app/admin/rx.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Rx do
 
+ permit_params :prescription_id, :drug, :dose, :quantity, :units, :details, :time_spread, :time_spread_unit
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
