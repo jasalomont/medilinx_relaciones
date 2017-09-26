1ca160a23b175a450cba68dc4e0a1ed525898bb7
Raghu Betina <raghu@firstdraft.com>
2017-09-26 01:47:07 +0000

    Generate doctors resource with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :doctors
+  # Routes for the Doctor resource:
+  # READ
+  get "/doctors", :controller => "doctors", :action => "index"
+  get "/doctors/:id", :controller => "doctors", :action => "show"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
